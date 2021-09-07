9dd35a6d82534ed274e70c82129ca0db70387fa7
Axel <axosar@gmail.com>
2021-09-07 17:11:23 -0500

    first commit

---
+++ pages/components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function Layout ({children}){
+  return(
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {children}
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </>
+  )
+}
