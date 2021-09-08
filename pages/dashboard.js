4716bc99a1e9fff9023acdf6e7f24054eb7dd236
Axel <axosar@gmail.com>
2021-09-07 20:13:02 -0500

    second api call

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 
 export default function Dashboard(props) {
 
-  if(!props.data){
+  if(!props.data && !props.data2){
     return(
       <div>
         <p>
@@ -14,6 +14,8 @@ export default function Dashboard(props) {
       </div>
     )
   };
+
+  console.log(props.data2);
   
   return (
     <Layout>
@@ -41,6 +43,17 @@ export default function Dashboard(props) {
                   <h1>$ {props.data.XRP.USD}</h1>
                 </td>
               </tr>
+              <tr>
+                <td>
+                  <h1>$ {props.data2[0].current_price}</h1>
+                </td>
+                <td>
+                  <h1>$ {props.data2[1].current_price}</h1>
+                </td>
+                <td>
+                  <h1>$ {props.data2[2].current_price}</h1>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
@@ -51,8 +64,10 @@ export default function Dashboard(props) {
 }
 
 export async function getStaticProps(context) {
-  const res = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=USD,EUR`)
-  const data = await res.json()
+  const res = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=USD,EUR`);
+  const res2 = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,ripple`)
+  const data = await res.json();
+  const data2 = await res2.json();
 
   if (!data) {
     return {
@@ -61,7 +76,10 @@ export async function getStaticProps(context) {
   }
 
   return {
-    props: { data }, // will be passed to the page component as props
+    props: { 
+      data,
+      data2
+    }, // will be passed to the page component as props
   }
 }
 
